ad2f18956f329b4775e8b6e03abd86cdb7a66450
Helarios <kuzinaksenya32@gmail.com>
2022-01-29 15:55:50 +0300

    feature/gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('div.gallery');
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('div.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img 
+                    class="gallery__image" 
+                    src="${preview}" 
+                    alt="${description}"
+                />
+            </a>
+        </div>`,
+    )
+    .join('');
+}
