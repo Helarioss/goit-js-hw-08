776d4bb00fb691000df04549937e6afde3a09079
Helarios <kuzinaksenya32@gmail.com>
2022-01-29 18:05:00 +0300

    feature/video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+player.setCurrentTime(localStorage.getItem(PLAYER_CURRENT_TIME));
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem(PLAYER_CURRENT_TIME, event.seconds);
+  }, 1000),
+);
